fc7ee1266b5e2ceaa66aeddc098fe569dfa6dc35
ko02 <kd1295315@st.kobedenshi.ac.jp>
2024-11-28 00:00:09 +0900

    YouTube

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+          勉強用        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          YouTube
         </a>
       </header>
     </div>
